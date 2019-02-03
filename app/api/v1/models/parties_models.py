dfdd6d6cadfe03febb082a951ba80642dca51af0
Willies Wanjala <gwiliez@gmail.com>
2019-02-03 21:36:39 +0300

    [start #163671165] write failing tests for party models

---
+++ app/api/v1/models/parties_models.py
@@ -0,0 +1,15 @@
+""" This module contains classes that handle party related data """
+
+# Standard imports
+import datetime
+
+# Local imports
+from flask import make_response, jsonify
+
+PARTY_DB = {
+    
+}
+
+class PartyModels:
+    """ Defines methods that handle operations regarding parties """
+    pass
