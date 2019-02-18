21a12925b6ff7d1ed4f66cc2c99209b284e34f49
Willies Wanjala <gwiliez@gmail.com>
2019-02-18 06:36:30 +0300

    host on heroku

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import os
 from app import create_app
 
 
-APP = create_app(os.getenv('FLASK_ENV'))
+app = create_app(os.getenv('FLASK_ENV'))
 
 if __name__ == "__main__":
-    APP.run()
+    app.run()
