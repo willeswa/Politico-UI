d9f4e7050037405999c53b0902e268ba89cfd2fc
Willies Wanjala <gwiliez@gmail.com>
2019-02-27 15:30:20 +0300

    [bug #164260009]send a create user request

--- UI/scripts/signup.js
+++ UI/scripts/signup.js
@@ -1,46 +1,65 @@
 const url = 'https://politiko-api.herokuapp.com/api/v2/auth/signup';
 let register = document.getElementById('register');
 
+window.localStorage.clear()
+if (window.localStorage.getItem('email') == null) {
+    document.getElementById('logbut').innerHTML = 'LOGIN';
+    document.getElementById('log-link').setAttribute('href', 'signin.html')
+}
+
 register.onclick = (event) => {
     event.preventDefault();
-    let firstname = document.getElementById('firstname');
-    let secondname = document.getElementById('secondname');
-    let othername = document.getElementById('othername');
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    let phone_number = document.getElementById('phone_number');
-    let passport_url = document.getElementById('passport_url');
+    let firstname = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let othername = document.getElementById('othername').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let phone_number = document.getElementById('phone_number').value;
+    let passport_url = document.getElementById('passport_url').value;
 
-    fetch(url, {
-        mode: 'cors',
+    let form_data = JSON.stringify({
+        firstname: firstname,
+        lastname: lastname,
+        othername: othername,
+        email: email,
+        password: password,
+        phone_number: phone_number,
+        passport_url: passport_url
+    })
+
+    let fetchData = {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
+        body: form_data,
+        headers: new Headers(
             {
-                firstname: firstname,
-                secondname: secondname,
-                othername: othername,
-                email: email,
-                password: password,
-                phone_number: phone_number,
-                passport_url: passport_url
+                'Content-Type': 'application/json'
             }
         )
-    })
-    .then(res => res.json())
-    .then(data => {
-        let success = data['data'],
-            error = data['error'];
-        
-        if (success) {
-            document.getElementById('error-message').innerHTML = '';
-            window.location.replace('dashboard.html')
-        }
-    })
-    .catch(error => {
-        console.log(error)
-    })
+    }
+
+    fetch(url, fetchData)
+        .then(res => res.json())
+
+        .then(data => {
+            let success = data['data'],
+                error = data['error'];
+
+            if (success) {
+                window.localStorage.setItem('email', email)
+                window.location.replace('signin.html')
+            }
+            else if (error) {
+                let error_p = document.getElementById('error');
+                error_p.innerHTML = error;
+                error_p.className += "error";
+                console.log(error)
+            }
+        })
+        .catch(error => {
+            // let error_p = document.getElementById('error');
+            // error_p.innerHTML = error;
+            // error_p.className += "error";
+            // console.log(error)
+        })
 
 }
