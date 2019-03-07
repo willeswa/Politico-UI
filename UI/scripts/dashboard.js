089c3aa163965259d6d6bdae818fb8e8c1c1586f
Willies Wanjala <gwiliez@gmail.com>
2019-03-07 10:59:10 +0300

    [finish #164406640] dynamically populate user dashboard with data

--- UI/scripts/dashboard.js
+++ UI/scripts/dashboard.js
@@ -3,19 +3,38 @@ const logoutBut = document.getElementById('logout'),
     profile = document.getElementById('profile'),
     editForm = document.getElementById('edit-form'),
     submitEdit = document.getElementById('submit-edit'),
-    cancel = document.getElementById('cancel');
+    cancel = document.getElementById('cancel'),
+    results = document.getElementById('vote-node'),
+    loggedInName = document.getElementById('loggedin-name'),
+    loggedEmail = document.getElementById('loggedin-email'),
+    loggedPhoneNumber = document.getElementById('loggedin-phone-number'),
+    loggedinPassport = document.getElementById('loggedin-passport'),
+    profIcon = document.getElementById('prof-icon');
 
 logout()
 
 if (window.localStorage.getItem('token') !== null) {
 
-    
+    document.title = 'POLITIKO | ' + window.localStorage.getItem('lastname');
+    const user_id = window.localStorage.getItem('user_id'),
+        user_email = window.localStorage.getItem('email'),
+        user_firstname = window.localStorage.getItem('firstname'),
+        user_lastname = window.localStorage.getItem('lastname'),
+        user_othername = window.localStorage.getItem('othername'),
+        userPassport = window.localStorage.getItem('passport_url'),
+        phoneNumber = window.localStorage.getItem('phone_number');
+
+        loggedInName.innerHTML = user_firstname + ' ' + user_othername + ' ' + user_lastname;
+        loggedEmail.innerHTML = user_email;
+        loggedPhoneNumber.innerHTML = phoneNumber;
+        loggedinPassport.setAttribute('src', userPassport);
+        // profIcon.setAttribute('src', userPassport);
 
     editProfileBut.onclick = (event) => {
         event.preventDefault()
         profile.style.display = 'none';
         editForm.style.display = 'block';
-        
+
     }
 
     submitEdit.onclick = (event) => {
@@ -29,14 +48,67 @@ if (window.localStorage.getItem('token') !== null) {
         editForm.style.display = 'none';
     }
 
+    console.log(user_id)
+
+    fetch('https://politiko-api.herokuapp.com/api/v2/users/' + user_id + '/history')
+        .then(response => response.json())
+        .then(data => {
+            success = data['data'];
+            error = data['error'];
+
+            voteHistory = data.data;
+
+            if (success) {
+                if (voteHistory.length == 0) {
+                    let nullResult = createNode('li');
+                    nullResult.innerHTML = 'You have no election history. Go ahead and vote for your favorite candidates and be part of the change.';
+                    nullResult.className += ('no-results-display result');
+                    append(results, nullResult);
+                } else {
+
+                    let voteNode = document.getElementById('vote-node');
+
+                    voteHistory.map(voteHistory => {
+                        let h2 = createNode('h2'),
+                            parentLi = createNode('li'),
+                            ol = createNode('ol'),
+                            childLi = createNode('li'),
+                            h3 = createNode('h3'),
+                            p = createNode('p');
+                        h2.innerHTML = voteHistory.office;
+                        h3.innerHTML = 'Voted: ' + voteHistory.candidate;
+                        parentLi.className += ('result');
+
+                        append(childLi, h3)
+                        append(childLi, p)
+                        append(ol, childLi)
+                        append(parentLi, h2)
+                        append(parentLi, ol)
+                        append(voteNode, parentLi)
+
+                    })
+                }
+            } else {
+                console.log(error)
+            }
+        })
+
 } else {
     window.location.replace('index.html');
 }
 
 function logout() {
-    logoutBut   .onclick = (event) => {
+    logoutBut.onclick = (event) => {
         event.preventDefault();
         window.localStorage.clear();
         window.location.replace('signin.html');
     }
+}
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, element) {
+    return parent.appendChild(element);
 }
--- UI/scripts/signin.js
+++ UI/scripts/signin.js
@@ -33,12 +33,28 @@ if (window.localStorage.getItem('email') !== null) {
                     error = data['error'];
 
                 if (success) {
-                    is_admin = data['data'][0]['user']['is_admin']
-                    token = data['data'][0]['token']
+                    console.log(data)
+                    let isAdmin = data['data'][0]['user']['is_admin'],
+                        token = data['data'][0]['token'],
+                        userId = data['data'][0]['user']['user_id'],
+                        email = data['data'][0]['user']['email'],
+                        firstname = data['data'][0]['user']['firstname'],
+                        lastname = data['data'][0]['user']['lastname'],
+                        othername = data['data'][0]['user']['othername'],
+                        phonNumber = data['data'][0]['user']['phone_number'],
+                        passportUrl = data['data'][0]['user']['passport_url'];
+
                     window.localStorage.setItem('token', token)
                     window.localStorage.setItem('email', email)
-                    window.localStorage.setItem('is_admin', is_admin)
-                    if (is_admin) {
+                    window.localStorage.setItem('is_admin', isAdmin)
+                    window.localStorage.setItem('user_id', userId)
+                    window.localStorage.setItem('firstname', firstname)
+                    window.localStorage.setItem('lastname', lastname)
+                    window.localStorage.setItem('phone_number', phonNumber)
+                    window.localStorage.setItem('othername', othername)
+                    window.localStorage.setItem('passport_url', passportUrl)
+
+                    if (isAdmin) {
                         window.location.replace('admin.html')
                     } else {
                         window.location.replace('dashboard.html')
